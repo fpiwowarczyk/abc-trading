6bf4c78e2d5ce6fb3b999d6475b98131e0e8a95e
Filip Piwowarczyk <filip.piwowarczyk1997@gmail.com>
2024-09-01 20:53:59 +0200

    Revert "Two stores"

--- main.go
+++ main.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/fpiwowarczyk/abc-trading/internal/config"
-	"github.com/fpiwowarczyk/abc-trading/internal/handler"
-	"github.com/fpiwowarczyk/abc-trading/internal/transactions"
-)
+import "fmt"
 
 func main() {
-
-	inMemStore := transactions.NewCustomInMemStore()
-
-	cfg := config.NewConfig()
-
-	srv := handler.NewHandler(
-		log.New(log.Writer(), "", log.LstdFlags),
-		cfg,
-		inMemStore,
-	)
-
-	httpSrv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", 8080), // TODO move it to config
-		Handler: srv,
-	}
-
-	// TODO Maybe move it to goroutine for grafeful shutdown
-	log.Printf("Server is listening on %s", httpSrv.Addr)
-	if err := httpSrv.ListenAndServe(); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
-
+	fmt.Println("heelo")
 }
